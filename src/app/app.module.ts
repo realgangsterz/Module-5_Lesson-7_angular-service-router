fdd1b2a2c44393f65d935053fd45a527ffdf09d8
daoxuandong1997 <daoxuandong1997@gmail.con>
2020-09-29 09:56:41 +0700

    timelines done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,31 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TimelinesComponent } from './timelines/timelines.component';
+import {PreloadAllModules, RouterModule, Routes} from '@angular/router';
 
+const routers: Routes = [
+  {
+    path: 'timelines',
+    component: TimelinesComponent
+  }
+];
+
+// @ts-ignore
+// @ts-ignore
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TimelinesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(routers, {
+      preloadingStrategy: PreloadAllModules
+    })
   ],
+  exports: [RouterModule],
   providers: [],
   bootstrap: [AppComponent]
 })
